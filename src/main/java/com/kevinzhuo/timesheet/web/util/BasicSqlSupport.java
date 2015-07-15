7f5243830fccae42a3e0fd6b73980011f3fdaf9a
Kevin <WANGZHUO648217@gmail.com>
2015-07-15 17:43:59 +0800

    MybatisDemo commit 2015/07/16 16:30

---
+++ src/main/java/com/kevinzhuo/timesheet/web/util/BasicSqlSupport.java
@@ -0,0 +1,16 @@
+package com.kevinzhuo.timesheet.web.util;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class BasicSqlSupport {
+
+	protected SqlSession session;
+
+	public SqlSession getSession() {
+		return session;
+	}
+
+	public void setSession(SqlSession session) {
+		this.session = session;
+	}
+}
